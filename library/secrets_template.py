d34275bce3a18da244d45494a39db75fd0eb0e66
hernandezb3 <brittney.hernandez@uconn.edu>
2025-05-13 10:07:04 -0400

    initial build

---
+++ library/secrets_template.py
@@ -0,0 +1,10 @@
+# template for adding api keys
+# assuming you are working from a cloned repository:
+# save a copy of the template in a file called secrets.py
+# this file is ignored in .gitignore
+# do NOT push the secrets.py online - the repository is public
+
+API_KEY = ""
+API_TOKEN = ""
+DATA_CENTER = "co1"
+SURVEY_NAME_ID = ""
